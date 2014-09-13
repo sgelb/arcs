44e1197249d8f11473c1b2a2decda0957abcd855
Colin <colin.sames@haw-hamburg.de>
2014-09-14 00:23:30 +0200

    Added move functions. Should work, needs testing though.

--- Arcs/src/com/github/sgelb/arcs/Face.java
+++ Arcs/src/com/github/sgelb/arcs/Face.java
@@ -29,8 +29,12 @@ public class Face {
 	
 	//##### getter #####
 	
-	public Color getSquare(int pos) {
-		return squares[pos];
+	public Color[] getSquaresAt(int[] pos) {
+		Color[] temp = new Color[3];
+		temp[0] = squares[pos[0]];
+		temp[1] = squares[pos[1]];
+		temp[2] = squares[pos[2]];
+		return temp;
 	}
 	
 	public Color[] getSquares() {
@@ -40,5 +44,41 @@ public class Face {
 	public Color[] getOldSquares() {
 		return oldSquares;
 	}
-
+	
+	
+	// Turns the whole face by 90/ -90 degree.
+	
+	public void turn90Deg(boolean reverse) {
+		oldSquares = squares;
+		
+		int counter = 9;
+		
+		if(!reverse) {
+			for(int i = 0; i < 9; i++) {
+				
+				/* this if-statement checks whether we are in bounds
+				 * of modulo 9 or not (kind-of-modulo space, not exactly but
+				 * it works). If not, add 7 to counter.
+				 * Moving in the modulo 9 space makes it more dynamic.
+				 */
+				if(counter - 3 >= 0) {
+					counter -= 3;
+				} else {
+					counter += 7;
+				}
+				squares[i] = oldSquares[counter];
+			}
+		} else {
+			for(int i = 9; i >= 0; i--) {
+				if(counter - 3 > 0) {
+					counter -= 3;
+				} else {
+					counter += 7;
+				}
+				squares[i] = oldSquares[counter];
+			}
+		}
+	}
+	
+	
 }
