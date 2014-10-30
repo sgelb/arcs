303c3ca6ec1d485264fff0ee807ed9b2d73531a7
sgelb <stephan.halbritter@haw-hamburg.de>
2014-10-30 16:30:02 +0100

    Fix bug in SettingsActivity.
    No more crashs when restarting from Settings

--- Arcs/src/com/github/sgelb/arcs/MainActivity.java
+++ Arcs/src/com/github/sgelb/arcs/MainActivity.java
@@ -193,6 +193,7 @@ public class MainActivity extends Activity implements CvCameraViewListener2, Obs
 				this, mLoaderCallback);
 	}
 
+	@Override
 	public void onDestroy() {
 		super.onDestroy();
 		if (mOpenCvCameraView != null)
